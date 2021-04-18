29f45e91c4842775db60ae8addecb15810fb33a9
Bharat Kumar <Bharkumar8@gmail.com>
2021-04-17 21:25:01 -0700

    Implemented the model for offers

---
+++ IbottaiOS/IbottaiOS/Model/Offers.swift
@@ -0,0 +1,36 @@
+//
+//  Offers.swift
+//  IbottaiOS
+//
+//  Created by Bharat Kumar on 4/17/21.
+//
+
+import Foundation
+
+struct Offer: Codable {
+    var id: String
+    var imageURL: String
+    var name: String
+    var description: String
+    var terms: String
+    var cashBack: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case imageURL = "url"
+        case name
+        case description
+        case terms
+        case cashBack = "current_value"
+    }
+    
+     init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        imageURL = try container.decode(String.self, forKey: .imageURL)
+        name = try container.decode(String.self, forKey: .name)
+        description = try container.decode(String.self, forKey: .description)
+        terms = try container.decode(String.self, forKey: .terms)
+        cashBack = try container.decode(String.self, forKey: .cashBack)
+    }
+}
